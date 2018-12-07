b48f6ba6668b77de0b7d15ac84d0db7e617bc4cd
Colin MacLeod <colinlmacleod1@gmail.com>
2018-12-07 01:39:34 -0500

    Added waypoints

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadFirstLevel", 5);
+    }
+
+    // Update is called once per frame
+    void Update () {
+    	
+    }
+
+    void LoadFirstLevel() { 
+    SceneManager.LoadScene(1);
+    }
+}
